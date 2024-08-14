179601bba8f1dbff764ef1fa964d2c38bec3836f
JunsoMin <minjs4562@gmail.com>
2024-08-14 10:09:17 +0900

    feat: pipeline

--- include/shark_path_handler/path_handling_node.hpp
+++ include/shark_path_handler/path_handling_node.hpp
@@ -2,6 +2,9 @@
 #define SHARK_PATH_HANDLER__PATH_HANDLING_NODE_H_
 
 #include <ros/ros.h>
+#include <sensor_msgs/NavSatFix.h>
+
+#include <tf/tf.h>
 
 class PathHandlingNode
 {
@@ -9,8 +12,16 @@ public:
   PathHandlingNode();
 
 private:
+  void gps_cb(const sensor_msgs::NavSatFix::ConstPtr& msg);
+  void calc_sudo_gps();
+
   ros::NodeHandle nh_;
   ros::NodeHandle priv_nh_;
+  
+  ros::Subscriber gps_subs_;
+  ros::Publisher sudo_gps_pub_;
 
+  sensor_msgs::NavSatFix sudo_gps_;
+  
 };
 #endif  // SHARK_PATH_HANDLER__PATH_HANDLING_NODE_H_
--- src/path_handling_node.cpp
+++ src/path_handling_node.cpp
@@ -1,7 +1,32 @@
 #include "shark_path_handler/path_handling_node.hpp"
 
-PathHandlingNode::PathHandlingNode(){
+PathHandlingNode::PathHandlingNode()
+:nh_(),priv_nh_("~")
+{
   ROS_INFO("constructed!!");
+  gps_subs_ = nh_.subscribe<sensor_msgs::NavSatFix>("/gps",10,&PathHandlingNode::gps_cb);
+  sudo_gps_pub_ = nh_.advertise<sensor_msgs::NavSatFix>("/sudo_gps",10);
+}
+
+void PathHandlingNode::gps_cb(const sensor_msgs::NavSatFix::ConstPtr& msg){
+  if( msg->altitude != 0.0 && msg->latitude != 0.0 and msg->longitude != 0.0){
+    return;
+  }
+
+  assert(!msg->altitude);
+  sudo_gps_.header.frame_id = "base_link";
+
+  calc_sudo_gps();
+
+  sudo_gps_pub_.publish(sudo_gps_);
+}
+
+void PathHandlingNode::calc_sudo_gps(){
+  // create gps from map->base_link tf
+  // longitude 1 == 90180m
+  // latitude 1 == 110940m
+
+  
 }
 
 int main(int argc, char * argv[])
