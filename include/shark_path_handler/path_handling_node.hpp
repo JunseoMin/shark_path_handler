7def87e910a4e158c1df5e4aba510cec05be81ee
JunsoMin <minjs4562@gmail.com>
2024-08-20 23:11:23 +0900

    feat: last dance

--- include/shark_path_handler/path_handling_node.hpp
+++ include/shark_path_handler/path_handling_node.hpp
@@ -3,17 +3,28 @@
 
 #include <ros/ros.h>
 #include <sensor_msgs/NavSatFix.h>
+#include <nav_msgs/Odometry.h>
 
 #include <tf/tf.h>
+#include <tf2/utils.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+
+#include <geometry_msgs/PoseWithCovariance.h>
+#include <geometry_msgs/TwistWithCovariance.h>
+
+#include <morai_msgs/GPSMessage.h>
 
 class PathHandlingNode
 {
 public:
-  PathHandlingNode();
+  PathHandlingNode(ros::NodeHandle nh_);
 
 private:
-  void gps_cb(const sensor_msgs::NavSatFix::ConstPtr& msg);
-  void calc_sudo_gps();
+  void odom_cb(const nav_msgs::Odometry::ConstPtr& msg);
+  void gps_cb(const morai_msgs::GPSMessage::ConstPtr& msg);
+  void timer_cb(const ros::TimerEvent&);
+  void set_odom();
 
   ros::NodeHandle nh_;
   ros::NodeHandle priv_nh_;
@@ -21,7 +32,20 @@ private:
   ros::Subscriber gps_subs_;
   ros::Publisher sudo_gps_pub_;
 
+  ros::Subscriber odom_subs_;
+  ros::Publisher odom_pub_;
+
   sensor_msgs::NavSatFix sudo_gps_;
-  
+
+  tf2_ros::Buffer tf_buffer_;
+  tf2_ros::TransformListener tf_listener_;
+
+  ros::Timer timer_;
+  geometry_msgs::PoseWithCovariance pose_;
+  geometry_msgs::TwistWithCovariance twist_;
+
+  nav_msgs::Odometry odom_;
+
+  bool black_flag_;
 };
 #endif  // SHARK_PATH_HANDLER__PATH_HANDLING_NODE_H_
--- src/path_handling_node.cpp
+++ src/path_handling_node.cpp
@@ -1,39 +1,63 @@
 #include "shark_path_handler/path_handling_node.hpp"
 
-PathHandlingNode::PathHandlingNode()
-:nh_(),priv_nh_("~")
+PathHandlingNode::PathHandlingNode(ros::NodeHandle nh_)
+:nh_(nh_),priv_nh_("~"),black_flag_(false),tf_buffer_(),tf_listener_(tf_buffer_)
 {
   ROS_INFO("constructed!!");
-  gps_subs_ = nh_.subscribe<sensor_msgs::NavSatFix>("/gps",10,&PathHandlingNode::gps_cb);
-  sudo_gps_pub_ = nh_.advertise<sensor_msgs::NavSatFix>("/sudo_gps",10);
+  odom_pub_ = nh_.advertise<nav_msgs::Odometry>("/odom",10);
+  odom_subs_ = nh_.subscribe<nav_msgs::Odometry>("/Odometry", 10, &PathHandlingNode::odom_cb, this);  
+
+  gps_subs_ = nh_.subscribe<morai_msgs::GPSMessage>("/gps",10, &PathHandlingNode::gps_cb, this);
+  timer_ = nh_.createTimer(ros::Duration(0.1), &PathHandlingNode::timer_cb, this);
+
+  set_odom();
 }
 
-void PathHandlingNode::gps_cb(const sensor_msgs::NavSatFix::ConstPtr& msg){
-  if( msg->altitude != 0.0 && msg->latitude != 0.0 and msg->longitude != 0.0){
+void PathHandlingNode::odom_cb(const nav_msgs::Odometry::ConstPtr& msg){
+  if (!black_flag_) //case not blackout
+  {
+    return;
+  }
+  
+  pose_ = msg->pose;
+  twist_ = msg->twist;
+}
+
+void PathHandlingNode::gps_cb(const morai_msgs::GPSMessage::ConstPtr& msg){
+  if( msg->altitude != 0.0 || msg->latitude != 0.0 || msg->longitude != 0.0){
+    black_flag_ = false;
+    ROS_INFO("stable.. return!");
+    return;
+  }
+  ROS_INFO("blackout situation!");
+  black_flag_ = true;
+}
+
+void PathHandlingNode::timer_cb(const ros::TimerEvent&){
+  if (!black_flag_)
+  {
     return;
   }
 
-  assert(!msg->altitude);
-  sudo_gps_.header.frame_id = "base_link";
-
-  calc_sudo_gps();
-
-  sudo_gps_pub_.publish(sudo_gps_);
+  odom_.header.stamp = ros::Time::now();
+  odom_.pose = pose_;
+  odom_.twist = twist_;
+  odom_pub_.publish(odom_);
 }
 
-void PathHandlingNode::calc_sudo_gps(){
-  // create gps from map->base_link tf
-  // longitude 1 == 90180m
-  // latitude 1 == 110940m
-
-  
+void PathHandlingNode::set_odom(){
+  odom_.child_frame_id = "base_link";
+  odom_.header.frame_id = "camera_init";
 }
 
 int main(int argc, char * argv[])
 {
   ros::init(argc, argv, "path_handling_node");
-  auto path_handling_node = PathHandlingNode();
+
+  ros::NodeHandle nh;
+  PathHandlingNode handling_node(nh);
   ros::spin();
   ros::shutdown();
+  
   return 0;
 }
